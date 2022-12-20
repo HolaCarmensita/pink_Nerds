0f20dcab8b2fc766a04036cd249d19de5e01ac0e
Mohamad Ali Hassan <mohamadalihassan99@outlook.com>
2022-12-20 14:37:21 +0100

    added a js file to saldo page.

---
+++ plånbok.js
@@ -0,0 +1,7 @@
+let saldoValue = document.querySelector('.saldo-value')
+
+let btnSaldo = document.querySelector('.btn-saldo').addEventListener('click' ,function(){
+    console.log('Hello World')
+})
+
+//Make the btnSaldo appear in saldoValue. 
