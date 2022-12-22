13dd8569aa17ea708f34eabb9df857e5a7b13060
Danblad <rogerdanblad@gmail.com>
2022-12-22 14:13:31 +0100

    Added description to jasmine-test

--- Jasmine/spec/saldotest.js
+++ Jasmine/spec/saldotest.js
@@ -1,5 +1,7 @@
-describe("add", () => {
-  it("Sets the content to a desired number", () => {
+// Testar funktionen add som vi använder på saldo-sidan
+
+describe("adds two numbers from an input", () => {
+  it("Uses addition on content from two inputs to get one number", () => {
     let num1 = 500;
     let num2 = 200;
     expect(add(num1, num2)).toBe(700);
